9e521b757b6d03ea406a6ab3b19b4f3075acaec5
musokeharun <79862040+musokeharun@users.noreply.github.com>
2021-06-21 01:06:12 +0300

    Update headers

--- utils/headers.js
+++ utils/headers.js
@@ -1,18 +1,18 @@
 //HEADERS FOR GRAFANA
 const headers = {
-    accept: "application/json, text/plain, */*",
+     "accept": "application/json, text/plain, */*",
     "accept-language": "en-UG,en-US;q=0.9,en;q=0.8",
+    "cache-control": "no-cache",
     "content-type": "application/json",
-    "sec-ch-ua":
-        '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    "pragma": "no-cache",
+    "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
     "sec-ch-ua-mobile": "?0",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
     "x-grafana-org-id": "1",
-    referrerPolicy: "no-referrer-when-downgrade",
-    cookie:
-        "_nss=1; SERVERID=middleware; PHPSESSIDMW=e9t60soqm3brvqdt4v8qdhg2kb; mw_grafana_user=etalemwa@albayanmedia-africa.com; mw_grafana_hash=KuwPr_NfLqrVkxm0VqrbWQ; mw_grafana_expires=1621065940",
+    "cookie": "mw_grafana_user=mkyeyune@albayanmedia-africa.com; _nss=1; SERVERID=middleware; mw_grafana_hash=dlhLpoxR_phX2dfZvCPRNw; mw_grafana_expires=1626815070; PHPSESSIDMW=ih15v6r5lqofutfo55l36i88f6"
 };
 
+   
 module.exports = headers;
